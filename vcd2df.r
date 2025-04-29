1bc1e43a2b444821ae10940ae6a7b53d2db71147
calvin <cd-public@users.noreply.github.com>
2025-04-28 23:18:02 -0700

    Update vcd2df.r

--- vcd2df.r
+++ vcd2df.r
@@ -1,31 +1,53 @@
-  get_vars <- function(fptr) {
-    line <- readLines(fptr, n = 1)
-    # vars will be the names of variables and their encodings
-    vars <- list()
-    while (!grepl("\\$enddefinitions", line)) {
-      if (grepl("var", line)) {
-        # get the name and encoding and save it, if novel
-        # we assume same named vars if different modules are redundant
-        parts <- strsplit(line, " ")[[1]]
-        if (!parts[4] %in% vars) {
-          vars[[parts[5]]] <- parts[4]
-        }
-      }
+#' Read a value change dump (VCD) file and convert it to a data frame.
+#' 
+#' @param f_name The path of a VCD (value change dump) file.
+#' @returns A data frame with the values of the variables at each time point.
+#' @examples
+#' vcd2df("example.vcd")
+#' vcd2df("testbench.vcd")
+vcd2df <- function(f_name) {
+  # Basically, do some hacks to read line-by-line, and skip to the good part (vars)
+  fptr <- file(f_name, "r")
+  line <- readLines(fptr, n = 1)
+  while (!grepl("\\$scope", line)) {
       line <- readLines(fptr, n = 1)
-    }
-    return(vars)
   }
 
-  tick <- function(fptr, t_n) {
+  # Read the variables and perform a few transposes
+  # We need vars to rename the rows latter, but easier to use aliases atm.
+  vars <- list()
+  while (!grepl("\\$enddefinitions", line)) {
+    if (grepl("var", line)) {
+      # get the name and encoding and save it, if novel
+      # we assume same named vars if different modules are redundant
+      parts <- strsplit(line, " ")[[1]]
+      if (!parts[4] %in% vars) {
+        vars[[parts[5]]] <- parts[4]
+      }
+    }
     line <- readLines(fptr, n = 1)
-    # basically, we read until we get an empty line or another timestamp
+  }
+  # We make a df to store a time point
+  t_n <- setNames(rep(-1, length(vars)), vars)
+  df <- data.frame(t_n)
+  
+  while (!grepl("\\$dumpvars", readLines(fptr, n = 1))) {
+    # Keep reading until $dumpvars is found
+  }
+
+  # Now we have the variables, we can read the dumpvars
+  # We know there's a #0 somewhere, so...
+  time <- '#0'
+  while (TRUE) {
     while (length(line) > 0 && substring(line, 1, 1) != "#") {
       # lines either store a bit or a word, we parse differently
-      if (grepl(" ", line)) { # word
+      if (grepl(" ", line)) { 
+        # word
         parts <- strsplit(substring(line, 2), " ")[[1]]
         val <- parts[1]
         var <- trimws(parts[2])
-      } else { # bit
+      } else { 
+        # bit
         val <- substring(line, 1, 1)
         var <- trimws(substring(line, 2))
       }
@@ -41,59 +63,24 @@
       }
       line <- readLines(fptr, n = 1)
     }
-    # not sure how R works, but basically either we read another time stamp or a nothing
-    # have to signal this to the calling function
-    # ?TODO - refactor this? don't actually know enough R to handle idiomatically
-    if (length(line) == 0) {
-      return(list(done=TRUE))
+    # We have to update or single tick df and our result df, I guess
+    df$vals <- t_n
+    # We name the new column by the old time, then update the time
+    names(df)[ncol(df)] <- time
+    time <- line
+    if (length(line) != 0) {
+      # If we got another timestamp, we proceed.
+      line <- readLines(fptr, n = 1)
     } else {
-      return(list(time=line, vals=t_n))
+      # Or we change from encodings back to reg names, then return
+      close(fptr)
+      rownames(df) <- names(vars)
+      return(df[, -1])
     }
   }
+}
 
-  vcd2df <- function(f_name) {
-      # Basically, do some hacks to read line-by-line, and skip to the good part (vars)
-      fptr <- file(f_name, "r")
-      line <- readLines(fptr, n = 1)
-      while (!grepl("\\$scope", line)) {
-          line <- readLines(fptr, n = 1)
-      }
-
-      # Read the variables and perform a few transposes
-      # We need ids to rename the rows latter, but easier to use aliases atm.
-      ids <- get_vars(fptr)
-      # We make a df to store a time point
-      t_n <- setNames(rep(-1, length(ids)), ids)
-      df <- data.frame(t_n)
-      
-      while (!grepl("\\$dumpvars", readLines(fptr, n = 1))) {
-        # Keep reading until $dumpvars is found
-        # ?TODO - grab the timescale here then transform the ticks?
-        # I don't think that ever matters (relative time is fine)
-      }
-
-      # Now we have the variables, we can read the dumpvars
-      # We know there's a #0 somewhere, so...
-      time <- '#0'
-      while (TRUE) {
-        list <- tick(fptr, t_n)
-        if (length(list) == 2) {
-          # We have to update or single tick df and our result df, I guess
-          t_n <- list$vals
-          df$vals <- list$vals
-          # We name the new column by the old time, then update the time
-          names(df)[ncol(df)] <- time
-          time <- list$time
-        } else {
-          # We change from encodings back to reg names, then return
-          close(fptr)
-          rownames(df) <- names(ids)
-          return(df[, -1])
-        }
-      }
-  }
-
-  if (!interactive()) {
+if (!interactive()) {
     args <- commandArgs(trailingOnly = TRUE)
     if (length(args) == 1) {
       df <- vcd2df(args[1])
@@ -102,4 +89,4 @@
     } else {
       cat("Usage: Rscript vcd_df.R <filename>.vcd\n")
     }
-  }
+}
